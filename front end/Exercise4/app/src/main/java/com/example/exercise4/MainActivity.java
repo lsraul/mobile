0e6dc2442db96ac12cd7670fe0d8838e82db7ce6
raul.lozano <100346708ralumnos.uc3m.es>
2019-05-26 22:09:56 +0300

    initial commit

---
+++ front end/Exercise4/app/src/main/java/com/example/exercise4/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.exercise4;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button click;
+    public  static TextView data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        data = (TextView) findViewById(R.id.data);
+        fetchData process = new fetchData();
+        process.execute();
+
+
+    }
+}
